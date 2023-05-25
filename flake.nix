284aa99997623485cac8401ec302f86409dbbf80
Ingo Ruhnke <grumbel@gmail.com>
2023-05-25 17:53:06 +0200

    Add gradle build file

--- flake.nix
+++ flake.nix
@@ -19,10 +19,19 @@
             src = ./.;
 
             buildPhase = ''
+              gradle jar
             '';
 
             installPhase = ''
+              mkdir -p $out/bin $out/lib/
+              cp -vi build/libs/bsevolution.jar $out/lib/
+              echo -e "#!/bin/sh\nexec ${pkgs.jre}/bin/java -jar $out/lib/bsevolution.jar" > $out/bin/bsevolution
+              chmod +x $out/bin/bsevolution
             '';
+
+            nativeBuildInputs = with pkgs; [
+              gradle
+            ];
 
             buildInputs = with pkgs; [
               openjdk
